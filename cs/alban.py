e26496bab31a38665c467d05f92742a2da4ea419
Alban de Crevoisier <albandecrevoisier@gmail.com>
2018-12-10 13:24:37 +0100

    Add complexity explanation to part 1.

--- cs/alban.py
+++ cs/alban.py
@@ -30,6 +30,24 @@ def simple_cluster(image, x, y, clusters):
 
     Replace the '1's of the cluster around (x, y) by '0's and add the
     corresponding pixel coordinates to the cluster.
+
+    Complexity: Let m*n be the dimensions of the matrix.
+    Since we need to visit every pixel, we know that the lower bounds
+    on complexity and memory size are O(m*n) in both cases.
+    This algorithm has two elemnts: on one hand, it visits every pixel
+    and checks wether it is a '1' or not. If it is, it visits its entire
+    cluster and appends it to the list of clusters. The complexity of
+    the algorithm is thus something in (m*n)*(complexity of exploring
+    the clusters). However, we notice that once a cluster has been
+    visited, each of its pixel has been set to '0', thus not prompting
+    further exploration. This means that a '0' is visited once and a '1'
+    at most twice (once if it's the element by which the cluster is
+    discovered, twice otherwise, as the '0' will be checked later on).
+    The amortized complexity of exploring the clusters yields an overall
+    complexity in O(m*n).
+    As for memory, the matrix and the list of clusters is stored. At
+    most, every pixel will be in a cluster, so the list of clusters can
+    go up to m*n pixels. The memory cost is thus 2*m*n, or O(m*n).
     """
     clusters[-1].append([x, y])
     image[y][x] = 0
