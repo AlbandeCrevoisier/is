565907df05dbd94bef4ae1e21709dc02b8113b5f
Alban de Crevoisier <albandecrevoisier@gmail.com>
2019-07-17 23:32:00 +0200

    Add pandas basic cleaning.

---
+++ qm/orfee.py
@@ -0,0 +1,16 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set()
+
+
+data = pd.read_csv("data.csv", index_col=0, parse_dates=['date']).dropna()
+data.drop(['index'], axis=1, inplace=True)
+data.drop(data[data['age'] < 0].index, inplace=True)
+data.drop(data[data['exp'] < 0].index, inplace=True)
+data.drop(data[data['note'] > 100].index, inplace=True)
+
+data[['age', 'note']] /= 100
+cat = ['cheveux', 'sexe', 'diplome', 'specialite', 'dispo']
+data = pd.get_dummies(data, columns=cat)
