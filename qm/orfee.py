23b2420e7ea042569dbd31bc048c295225b9ff5d
Alban de Crevoisier <albandecrevoisier@gmail.com>
2019-07-21 20:22:21 +0200

    Use dates as index.

--- qm/orfee.py
+++ qm/orfee.py
@@ -5,11 +5,13 @@ import seaborn as sns
 sns.set()
 
 
-data = pd.read_csv("data.csv", index_col=0, parse_dates=['date']).dropna()
+data = pd.read_csv("data.csv", index_col='date', parse_dates=['date']).dropna()
+data.drop(['Unnamed: 0'], axis=1, inplace=True)
 data.drop(['index'], axis=1, inplace=True)
 data.drop(data[data['age'] < 0].index, inplace=True)
 data.drop(data[data['exp'] < 0].index, inplace=True)
 data.drop(data[data['note'] > 100].index, inplace=True)
+
 
 data[['age', 'note']] /= 100
 cat = ['cheveux', 'sexe', 'diplome', 'specialite', 'dispo']
