354eb9546075e48a1b22716f09782ec094d0b1dc
Alban de Crevoisier <albandecrevoisier@gmail.com>
2019-07-22 10:42:05 +0200

    Put code in functions to be used in REPL.

--- qm/orfee.py
+++ qm/orfee.py
@@ -5,14 +5,19 @@ import seaborn as sns
 sns.set()
 
 
-data = pd.read_csv("data.csv", index_col='date', parse_dates=['date']).dropna()
-data.drop(['Unnamed: 0'], axis=1, inplace=True)
-data.drop(['index'], axis=1, inplace=True)
-data.drop(data[data['age'] < 0].index, inplace=True)
-data.drop(data[data['exp'] < 0].index, inplace=True)
-data.drop(data[data['note'] > 100].index, inplace=True)
-
-
-data[['age', 'note']] /= 100
 cat = ['cheveux', 'sexe', 'diplome', 'specialite', 'dispo']
-data = pd.get_dummies(data, columns=cat)
+
+
+def load_data():
+	d = pd.read_csv("data.csv", index_col='date', parse_dates=['date']).dropna()
+	d.drop(['Unnamed: 0'], axis=1, inplace=True)
+	d.drop(['index'], axis=1, inplace=True)
+	d.drop(d[d['age'] < 0].index, inplace=True)
+	d.drop(d[d['exp'] < 0].index, inplace=True)
+	d.drop(d[d['note'] > 100].index, inplace=True)
+	return d
+
+
+def pp(d):
+	d[['age', 'note']] /= 100
+	d = pd.get_dummies(d, columns=cat)
