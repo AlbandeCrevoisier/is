c3abbbd318d2e60b47f6fbf74c4b83094faa7fef
Alban de Crevoisier <albandecrevoisier@gmail.com>
2019-07-22 11:52:25 +0200

    Clean up python helper.

--- qm/orfee.py
+++ qm/orfee.py
@@ -5,19 +5,17 @@ import seaborn as sns
 sns.set()
 
 
-cat = ['cheveux', 'sexe', 'diplome', 'specialite', 'dispo']
-
-
 def load_data():
 	d = pd.read_csv("data.csv", index_col='index', parse_dates=['date']).dropna()
 	d.drop(['Unnamed: 0'], axis=1, inplace=True)
 	d.drop(d[d['age'] < 0].index, inplace=True)
 	d.drop(d[d['exp'] < 0].index, inplace=True)
 	d.drop(d[d['note'] > 100].index, inplace=True)
-	t = d.pop('embauche')
-	return d, t
+	d.sort_values('date', inplace=True)
+	return d
 
 
 def pp(d):
+	cat = ['cheveux', 'sexe', 'diplome', 'specialite', 'dispo']
 	d[['age', 'note']] /= 100
 	d = pd.get_dummies(d, columns=cat)
