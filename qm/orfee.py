2c4240923fd2b40fff1c1ff86209e0ad3adf5cfa
Alban de Crevoisier <albandecrevoisier@gmail.com>
2019-07-22 10:59:36 +0200

    Do not index by dates, as they are not unique...

--- qm/orfee.py
+++ qm/orfee.py
@@ -9,13 +9,13 @@ cat = ['cheveux', 'sexe', 'diplome', 'specialite', 'dispo']
 
 
 def load_data():
-	d = pd.read_csv("data.csv", index_col='date', parse_dates=['date']).dropna()
+	d = pd.read_csv("data.csv", index_col='index', parse_dates=['date']).dropna()
 	d.drop(['Unnamed: 0'], axis=1, inplace=True)
-	d.drop(['index'], axis=1, inplace=True)
 	d.drop(d[d['age'] < 0].index, inplace=True)
 	d.drop(d[d['exp'] < 0].index, inplace=True)
 	d.drop(d[d['note'] > 100].index, inplace=True)
-	return d
+	t = d.pop('embauche')
+	return d, t
 
 
 def pp(d):
