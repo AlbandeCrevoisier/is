eb57f2be8364e905d6b4ee8cc4f4cc2824ae80cc
Alban de Crevoisier <albandecrevoisier@gmail.com>
2019-07-23 00:01:36 +0200

    Correct docstrings format to follow PEP-257.

--- qm/orfee.py
+++ qm/orfee.py
@@ -8,7 +8,7 @@ sns.set()
 
 
 def load_data():
-    """ Read data.csv and return a pandas DataFrame. """
+   """Read data.csv and return a pandas DataFrame."""
     d = pd.read_csv("data.csv", index_col='index', parse_dates=['date'])
     d.dropna(inplace=True)
     d.drop('Unnamed: 0', axis=1, inplace=True)
@@ -20,8 +20,8 @@ def load_data():
 
 
 def plots(d):
-    """ Relevant Seaborn plots. """
-    print("Taux d'embauche : ", d['embauche'].mean())
+   """Relevant Seaborn plots."""
+    print("Taux d'embauche :", d['embauche'].mean())
     sns.lineplot(data=d[['date', 'embauche']].groupby('date').sum().cumsum())
     sns.jointplot('age', 'exp', kind='kde', data=d)
     sns.kdeplot(d['salaire'], shade=True)
@@ -32,12 +32,15 @@ def plots(d):
 
 
 def pp(d, test_size=0.25):
-    """ Preprocess the DataFrame d and return a train/test split.
-    d: DataFrame
-    test_size: fraction of the test/train split
+   """Preprocess the DataFrame d and return a train/test split.
 
-    return: X_train, X_test, y_train, y_test as DataFrames.
-    """
+    Keyword arguments:
+    d -- DataFrame
+    test_size -- fraction of the test/train split
+
+    Returns:
+    X_train, X_test, y_train, y_test -- DataFrames.
+   """
     y = d.pop('embauche')
     X = d
     X['day'] = X['date'].transform(lambda x: x.day)
