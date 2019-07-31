8d2096c9382caedfa167549b228eb7260a822591
Alban de Crevoisier <albandecrevoisier@gmail.com>
2019-07-31 12:22:50 +0200

    Do not drop underage applications, as it seems to be valid data, sadly.

--- qm/orfee.py
+++ qm/orfee.py
@@ -27,7 +27,7 @@ def load_data():
     d = pd.read_csv("data.csv", index_col='index', parse_dates=['date'])
     d.dropna(inplace=True)
     d.drop('Unnamed: 0', axis=1, inplace=True)
-    d.drop(d[d['age'] < 18].index, inplace=True)
+    d.drop(d[d['age'] < 0].index, inplace=True)
     d.drop(d[d['exp'] < 0].index, inplace=True)
     d.drop(d[d['note'] > 100].index, inplace=True)
     d.sort_values('date', inplace=True)
